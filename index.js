d25226130b875f9f7babf647a1118229e0e79f7d
RabayaSultana <rabayashova811@gmail.com>
2024-04-30 14:21:35 +0600

    Delete CraftItem done

--- index.js
+++ index.js
@@ -92,6 +92,15 @@ app.get('/myCraftList/:email', async(req, res) => {
       res.send(result);
 })
 
+// Delete
+app.delete('/delete/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await craftItemCollection.deleteOne(query);
+  console.log(result)
+  res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
